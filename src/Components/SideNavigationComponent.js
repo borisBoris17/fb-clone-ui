4af119f10dd00b98d79067e6e3d8681550806cfa
Tucker <borisblade17@gmail.com>
2022-11-11 23:56:47 -0600

    add side bar navigation functionality - Tucker

--- src/Components/SideNavigationComponent.js
+++ src/Components/SideNavigationComponent.js
@@ -1,19 +1,25 @@
 import {React} from 'react';
 import {Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText} from '@mui/material';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import {Home} from '@mui/icons-material';
+import {Home, AccountBox} from '@mui/icons-material';
 
 function SideNavigationComponent() {
   return (
       <Box>
         <List>
-          <ListItem  variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <ListItemButton >
+          <ListItem variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <ListItemButton href="/">
               <ListItemIcon>
                 <Home />
               </ListItemIcon>
               <ListItemText primary="Home" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <ListItemButton  href="/Profile" >
+              <ListItemIcon>
+                <AccountBox />
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
             </ListItemButton>
           </ListItem>
         </List>
