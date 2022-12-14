98b0671a2c8c693dd0c47705747670d7b609992c
Tucker <borisblade17@gmail.com>
2022-12-13 23:46:02 -0600

    add the ability to edit hobbies on a profile - Tucker

--- src/Components/Profile/EditProfileDataComponent.js
+++ src/Components/Profile/EditProfileDataComponent.js
@@ -1,5 +1,6 @@
 import { React } from 'react';
 import { Typography, ListItem, ListItemIcon, TextField } from '@mui/material';
+import EditListComponent from './EditListComponent';
 
 function EditProfileDataComponent({ icon: Icon, title, fieldName, dataValue, handleEditProfileDataDetail }) {
 
@@ -7,28 +8,33 @@ function EditProfileDataComponent({ icon: Icon, title, fieldName, dataValue, han
     handleEditProfileDataDetail(fieldName, event.target.value);
   }
 
+  const handleUpdateHobbies = (newHobbies) => {
+    // const newHobies = [...dataValue, event.target.value];
+    handleEditProfileDataDetail(fieldName, newHobbies);
+  }
+
   return (
     <ListItem className='profileDataListItem'>
-        <ListItemIcon sx={{
-          minWidth: '40px',
-        }}>
-          <Icon />
-        </ListItemIcon>
-        <Typography
-          noWrap
-          sx={{
-            fontSize: "14pt",
-            fontWeight: "bold",
-            color: "#808080",
-            textAlign: "left",
-            flexBasis: "8em"
-          }}>{title}
-        </Typography>
-        <TextField 
-          className='editProfileData'
-          value={dataValue} 
-          onChange={handleUpdate}/>
-      </ListItem>
+      <ListItemIcon sx={{
+        minWidth: '40px',
+      }}>
+        <Icon />
+      </ListItemIcon>
+      <Typography
+        noWrap
+        sx={{
+          fontSize: "14pt",
+          fontWeight: "bold",
+          color: "#808080",
+          textAlign: "left",
+          flexBasis: "8em"
+        }}>{title}
+      </Typography>
+      {title === 'Hobbies' ? <EditListComponent hobbies={dataValue} handleUpdateHobbies={handleUpdateHobbies} /> : <TextField
+        className='editProfileData'
+        value={dataValue}
+        onChange={handleUpdate} />}
+    </ListItem>
   )
 }
 
